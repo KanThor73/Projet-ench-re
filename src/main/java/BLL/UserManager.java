9a53e7dbc470a690e826861484d1289b644f12da
KanThor73 <you@example.com>
2023-07-17 16:13:54 +0200

    Correction UserMAnager

--- src/main/java/BLL/UserManager.java
+++ src/main/java/BLL/UserManager.java
@@ -9,7 +9,7 @@ import DAL.Factory;
 public class UserManager {
 
 		public static void main (String []args) {
-			DAO userDAO = Factory.getUserDAO();
+			DAO<User> userDAO = Factory.getUserDAO();
 			List<User> userList = userDAO.selectAll();
 			for (User user : userList) {
 				System.out.println(user.toString());
--- src/main/java/DAL/Factory.java
+++ src/main/java/DAL/Factory.java
@@ -4,7 +4,6 @@ import BO.User;
 
 public class Factory {
 
-	
 	public static DAO<User> getUserDAO(){
 		return new UsersDAOimplJDBC();
 	}
