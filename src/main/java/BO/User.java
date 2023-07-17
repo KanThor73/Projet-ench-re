e29fd962c26e9ac4b5f289e19bb41e87b04ce042
Sam <sami.grolleau@mailo.com>
2023-07-17 15:08:39 +0200

    Classe Users BO : juste les attributs

---
+++ src/main/java/BO/User.java
@@ -0,0 +1,17 @@
+package BO;
+
+public class User {
+	
+	private int noUser;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
+	private boolean administrateur;
+}
