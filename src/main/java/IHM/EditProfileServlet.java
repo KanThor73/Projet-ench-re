8bc4c9e686c6e93075570afb44a09c4a94cc40fa
Jean Baptiste SABOURIN <136092521+jbsab@users.noreply.github.com>
2023-07-20 15:34:59 +0200

    Création EditProfile plus Servlet

---
+++ src/main/java/IHM/EditProfileServlet.java
@@ -0,0 +1,38 @@
+package IHM;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EditProfileServlet
+ */
+@WebServlet("/EditProfileServlet")
+public class EditProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+     
+    public EditProfileServlet() {
+        super();
+     
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/JSP/EditProfile.jsp").forward(request,response);
+		
+	}
+
+	
+
+	 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
