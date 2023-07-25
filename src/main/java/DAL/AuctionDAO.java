351b93a53a7546593f00d48090a115252a39db63
Sam <sami.grolleau@mailo.com>
2023-07-25 11:29:46 +0200

    requêtes SQL de Auction

--- src/main/java/DAL/AuctionDAO.java
+++ src/main/java/DAL/AuctionDAO.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import BO.Auction;
 import Exceptions.DALException;
-import Exceptions.BLLException;
 
 public interface AuctionDAO {
 	
 	public List<Auction> selectAll() throws DALException;
 	public Auction selectByID(int id) throws DALException;
-	public void update(Auction t) throws DALException, BLLException;
+	public void update(Auction t) throws DALException;
 	public void delete(int id) throws DALException;
-	public void insert(Auction t) throws DALException, BLLException;
+	public void insert(Auction t) throws DALException;
 }
