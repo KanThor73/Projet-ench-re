cc98994e75ba53fe47f5d615b2f5041d063709dd
Jean Baptiste SABOURIN <136092521+jbsab@users.noreply.github.com>
2023-07-25 11:16:34 +0200

    Connexion a la bdd

--- src/main/java/DAL/AuctionDAOimplJDBC.java
+++ src/main/java/DAL/AuctionDAOimplJDBC.java
@@ -1,41 +1,77 @@
 package DAL;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.mysql.jdbc.Connection;
+
+import BO.Article;
 import BO.Auction;
 import Exceptions.BLLException;
 import Exceptions.DALException;
 
 public class AuctionDAOimplJDBC implements AuctionDAO {
+	
+	public static final String AUCTION_SQL_SELECTALL = "SELECT * FROM Auctions";
+	public static final String AUCTION_SQL_SELECTBYID = "SELECT * FROM Auctions WHERE id = ?";
+	public static final String AUCTION_SQL_UPDATE = "UPDATE Auctions SET name = ?, description = ?, date_debut = ?, date_fin = ?, prix_initial = ?, prix_vente = ?, id_utilisateur = ?, gagnant = ? WHERE id = ?";
+	public static final String AUCTION_SQL_DELETE = "DELETE FROM Auctions WHERE id = ?";
+	public static final String AUCTION_SQL_INSERT = "INSERT INTO Auctions (name, description, date_debut, date_fin, prix_initial, prix_vente, id_utilisateur, gagnant) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
 	@Override
 	public List<Auction> selectAll() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		  try (Connection cnx = ConnectionProvider.getConnection()) {
+	            List<Auction> auctions = new ArrayList<>();
+	            PreparedStatement stmt = cnx.prepareStatement(AUCTION_SQL_SELECTALL);
+	            ResultSet rs = stmt.executeQuery();
+	          		
+		  }
 	}
+	       
+	            
+	            
+	        
+	    
 
 	@Override
 	public Auction selectByID(int id) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+
+			PreparedStatement stmt = cnx.prepareStatement(AUCTION_SQL_SELECTBYID);
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+		
+	}
 	}
 
 	@Override
 	public void update(Auction t) throws DALException, BLLException {
-		// TODO Auto-generated method stub
+		try (Connection cnx = ConnectionProvider.getConnection()) {
 
+			PreparedStatement stmt = cnx.prepareStatement(AUCTION_SQL_UPDATE);
+		}
 	}
 
 	@Override
 	public void delete(int id) throws DALException {
-		// TODO Auto-generated method stub
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+
+			PreparedStatement stmt = cnx.prepareStatement(AUCTION_SQL_DELETE);
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+		}
 
 	}
 
 	@Override
 	public void insert(Auction t) throws DALException, BLLException {
-		// TODO Auto-generated method stub
+		try (Connection connection = ConnectionProvider.getConnection()) {
+			PreparedStatement stmt = connection.prepareStatement(AUCTION_SQL_INSERT);
+
 
 	}
 
+	}
 }
